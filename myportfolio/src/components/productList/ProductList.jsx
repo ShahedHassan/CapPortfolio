311186ca83426ed481aaeb6592f513770a3f740b
Shade Hassan <Shade@MacBook-Pro-8.lan>
2022-08-31 13:27:06 -0500

    changes

---
+++ myportfolio/src/components/productList/ProductList.jsx
@@ -0,0 +1,23 @@
+import "./productList.css";
+import Product from "../product/Product";
+import { products } from "../../data";
+
+const ProductList = () => {
+  return (
+    <div className="pl">
+      <div className="pl-texts">
+        <h1 className="pl-title">Inspiration to Create</h1>
+        <p className="pl-desc">
+          Creative portfolio built with a diversity of teams, take a look!
+        </p>
+      </div>
+      <div className="pl-list">
+        {products.map((item) => (
+          <Product key={item.id} img={item.img} link={item.link} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
